b6e73d8e6e955e41cd894c35ca0c57faa97e9274
mwilliams659 <63402316+mwilliams659@users.noreply.github.com>
2020-11-11 15:47:36 +0000

    Styling and JS functionality
    Functionality that means that a box cannot be re-clicked or changed once it has been clicked.
    Styling added such as font-size and font-family.

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function place(box) {
     alert("You clicked here!");
     }
+
+
+
+// When clicked, the box will display an X or an O
+// function place(box) {
+//     box.innerText = currentPlayer;
+//     if (currentPlayer == "O") {
+//         currentPlayer = "X";
+//     } else {
+//         currentPlayer = "O"
+//     }
+// }
+
+var currentPlayer = "O";
+// This function lets user click the box but once clicked, it cannot be changed
+function place(box) {
+    if(box.innerText != "") return;
+    box.innerText = currentPlayer;
+    currentPlayer == "O" ? currentPlayer = "X" : currentPlayer = "O";
+}
